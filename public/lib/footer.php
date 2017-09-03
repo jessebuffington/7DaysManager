434fa2a8c04d97e3f8429e481a0d2ccf92298fa6
Jesse Buffington <jesse.buffington@scientificgames.com>
2017-09-03 09:30:02 -0400

    Change for new template

--- public/lib/footer.php
+++ public/lib/footer.php
@@ -1,63 +1,218 @@
-<footer class="page-footer center-on-small-only">
-   <!--Footer Links-->
-   <div class="container-fluid">
-      <div class="row">
-         <!--First column-->
-         <div class="col-md-3 offset-lg-1 hidden-lg-down">
-            <h5 class="title">ABOUT MATERIAL DESIGN</h5>
-            <p>Material Design (codenamed Quantum Paper) is a design language developed by Google. </p>
-            <p>Material Design for Bootstrap (MDB) is a powerful Material Design UI KIT for most popular HTML, CSS, and JS framework - Bootstrap.</p>
-         </div>
-         <!--/.First column-->
-         <hr class="hidden-md-up">
-         <!--Second column-->
-         <div class="col-lg-2 col-md-4 offset-lg-1">
-            <h5 class="title">Our projects</h5>
-            <ul>
-               <li><a href="#!">Line-two</a></li>
-               <li><a href="#!">Dreamlink Inc.</a></li>
-               <li><a href="#!">Vasty&Broad</a></li>
-               <li><a href="#!">Koromba Ltd.</a></li>
-            </ul>
-         </div>
-         <!--/.Second column-->
-         <hr class="hidden-md-up">
-         <!--Third column-->
-         <div class="col-lg-2 col-md-4">
-            <h5 class="title">Contact</h5>
-            <ul>
-               <li><a href="#!">Social Media</a></li>
-               <li><a href="#!">Sales</a></li>
-               <li><a href="#!">Support</a></li>
-               <li><a href="#!">Contact details</a></li>
-            </ul>
-         </div>
-         <!--/.Third column-->
-         <hr class="hidden-md-up">
-         <!--Fourth column-->
-         <div class="col-lg-2 col-md-4">
-            <h5 class="title">Culture</h5>
-            <ul>
-               <li><a href="#!">Events</a></li>
-               <li><a href="#!">People</a></li>
-               <li><a href="#!">Jobs</a></li>
-               <li><a href="#!">Plans</a></li>
-            </ul>
-         </div>
-         <!--/.Fourth column-->
-      </div>
-   </div>
-   <!--/.Footer Links-->
-   <!--Copyright-->
-   <div class="footer-copyright">
-      <div class="container-fluid">
-         © 2017 Copyright: Jesse Buffington
-      </div>
-   </div>
-   <!--/.Copyright-->
+<footer class="main-footer">
+  <div class="pull-right hidden-xs">
+    <b>
+      Version
+    </b>
+     2.4.0
+  </div>
+  <strong>
+    Copyright &copy; 2014-2016
+    <a href="https://adminlte.io">
+    Almsaeed Studio
+  </a>
+  .
+</strong>
+All rights reserved.
 </footer>
-<!--/.Footer-->
-<script src='/js/jquery-3.2.1.min.js'></script>
-<script src='/js/bootstrap.js'></script>
-<script src='/js/mdb.min.js'></script>
-<script src="/js/index.js"></script>
+<aside class="control-sidebar control-sidebar-dark">
+<ul class="nav nav-tabs nav-justified control-sidebar-tabs">
+  <li>
+    <a href="#control-sidebar-home-tab" data-toggle="tab">
+      <i class="fa fa-home"></i>
+    </a>
+  </li>
+  <li>
+    <a href="#control-sidebar-settings-tab" data-toggle="tab">
+      <i class="fa fa-gears"></i>
+    </a>
+  </li>
+</ul>
+<div class="tab-content">
+  <div class="tab-pane" id="control-sidebar-home-tab">
+    <h3 class="control-sidebar-heading">
+      Recent Activity
+    </h3>
+    <ul class="control-sidebar-menu">
+      <li>
+        <a href="javascript:void(0)">
+          <i class="menu-icon fa fa-birthday-cake bg-red"></i>
+          <div class="menu-info">
+            <h4 class="control-sidebar-subheading">
+              Langdon's Birthday
+            </h4>
+            <p>
+              Will be 23 on April 24th
+            </p>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">
+          <i class="menu-icon fa fa-user bg-yellow"></i>
+          <div class="menu-info">
+            <h4 class="control-sidebar-subheading">
+              Frodo Updated His Profile
+            </h4>
+            <p>
+              New phone +1(800)555-1234
+            </p>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">
+          <i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
+          <div class="menu-info">
+            <h4 class="control-sidebar-subheading">
+              Nora Joined Mailing List
+            </h4>
+            <p>
+              nora@example.com
+            </p>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">
+          <i class="menu-icon fa fa-file-code-o bg-green"></i>
+          <div class="menu-info">
+            <h4 class="control-sidebar-subheading">
+              Cron Job 254 Executed
+            </h4>
+            <p>
+              Execution time 5 seconds
+            </p>
+          </div>
+        </a>
+      </li>
+    </ul>
+    <h3 class="control-sidebar-heading">
+      Tasks Progress
+    </h3>
+    <ul class="control-sidebar-menu">
+      <li>
+        <a href="javascript:void(0)">
+          <h4 class="control-sidebar-subheading">
+            Custom Template Design
+            <span class="label label-danger pull-right">
+              70%
+            </span>
+          </h4>
+          <div class="progress progress-xxs">
+            <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">
+          <h4 class="control-sidebar-subheading">
+            Update Resume
+            <span class="label label-success pull-right">
+              95%
+            </span>
+          </h4>
+          <div class="progress progress-xxs">
+            <div class="progress-bar progress-bar-success" style="width: 95%"></div>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">
+          <h4 class="control-sidebar-subheading">
+            Laravel Integration
+            <span class="label label-warning pull-right">
+              50%
+            </span>
+          </h4>
+          <div class="progress progress-xxs">
+            <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a href="javascript:void(0)">
+          <h4 class="control-sidebar-subheading">
+            Back End Framework
+            <span class="label label-primary pull-right">
+              68%
+            </span>
+          </h4>
+          <div class="progress progress-xxs">
+            <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div class="tab-pane" id="control-sidebar-settings-tab">
+    <form method="post">
+      <h3 class="control-sidebar-heading">
+        General Settings
+      </h3>
+      <div class="form-group">
+        <label class="control-sidebar-subheading">
+          Report panel usage
+          <input type="checkbox" class="pull-right" checked>
+        </label>
+        <p>
+          Some information about this general settings option
+        </p>
+      </div>
+      <div class="form-group">
+        <label class="control-sidebar-subheading">
+          Allow mail redirect
+          <input type="checkbox" class="pull-right" checked>
+        </label>
+        <p>
+          Other sets of options are available
+        </p>
+      </div>
+      <div class="form-group">
+        <label class="control-sidebar-subheading">
+          Expose author name in posts
+          <input type="checkbox" class="pull-right" checked>
+        </label>
+        <p>
+          Allow the user to show his name in blog posts
+        </p>
+      </div>
+      <h3 class="control-sidebar-heading">
+        Chat Settings
+      </h3>
+      <div class="form-group">
+        <label class="control-sidebar-subheading">
+          Show me as online
+          <input type="checkbox" class="pull-right" checked>
+        </label>
+      </div>
+      <div class="form-group">
+        <label class="control-sidebar-subheading">
+          Turn off notifications
+          <input type="checkbox" class="pull-right">
+        </label>
+      </div>
+      <div class="form-group">
+        <label class="control-sidebar-subheading">
+          Delete chat history
+          <a href="javascript:void(0)" class="text-red pull-right">
+            <i class="fa fa-trash-o"></i>
+          </a>
+        </label>
+      </div>
+    </form>
+  </div>
+</div>
+</aside>
+<div class="control-sidebar-bg"></div>
+</div>
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="bower_components/fastclick/lib/fastclick.js"></script>
+<script src="dist/js/adminlte.min.js"></script>
+<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="bower_components/Chart.js/Chart.js"></script>
+<script src="dist/js/pages/dashboard2.js"></script>
+<script src="dist/js/demo.js"></script>
