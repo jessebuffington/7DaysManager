71cdecd2c38d418e61f12ac7b690868b051a52ae
Jesse Buffington <Bassmastry101@gmail.com>
2018-01-03 08:25:10 -0500

    Init new page

---
+++ public/pages/reports/playerReport.php
@@ -0,0 +1,64 @@
+<?php
+  require($_SERVER["DOCUMENT_ROOT"] . "/lib/loginHeader.php");
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php
+      $pageTitle='Player Report';
+      $pageParent='Reports';
+      include_once($_SERVER[ "DOCUMENT_ROOT"] . "/lib/header.php");
+    ?>
+  </head>
+  <body class="hold-transition skin-<?php echo HEADER_COLOR ?> sidebar-mini">
+    <div class="wrapper">
+      <?php
+        include_once($_SERVER[ "DOCUMENT_ROOT"] . "/lib/sidebar.php");
+      ?>
+  <div class="content-wrapper">
+    <section class="content-header">
+      <h1>
+        Blank page
+        <small>
+          Version <?php echo SITE_VERSION;?>
+        </small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Examples</a></li>
+        <li class="active">Blank page</li>
+      </ol>
+    </section>
+    <section class="content">
+      <div class="box">
+        <div class="box-header with-border">
+          <h3 class="box-title">Title</h3>
+
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                    title="Collapse">
+              <i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+              <i class="fa fa-times"></i></button>
+          </div>
+        </div>
+        <div class="box-body">
+          Start creating your amazing application!
+        </div>
+        <div class="box-footer">
+          Footer
+        </div>
+      </div>
+    </section>
+  </div>
+  <?php
+    include_once($_SERVER[ "DOCUMENT_ROOT"] . "/lib/footer.php");
+  ?>
+<script>
+  $(document).ready(function () {
+    $('.sidebar-menu').tree()
+  })
+</script>
+</body>
+</html>
