d01fad2262de7b7c2cdc0630f18916c4c6191339
Jesse Buffington <Bassmastry101@gmail.com>
2018-01-07 09:15:27 -0500

    Remove version at top of page. Set dynamic page name.

--- public/index.php
+++ public/index.php
@@ -22,20 +22,10 @@
         <section class="content-header">
           <h1>
             Dashboard
-            <small>
-              Version <?php echo SITE_VERSION;?>
-            </small>
           </h1>
           <ol class="breadcrumb">
-            <li>
-              <a href="/">
-                <i class="fa fa-dashboard"></i>
-                 Home
-              </a>
-            </li>
-            <li class="active">
-              Dashboard
-            </li>
+            <li><a href="/"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active"><?php echo $pageTitle ?></li>
           </ol>
         </section>
         <section class="content">
