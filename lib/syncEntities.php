26b6273ceae3d42e3d92e881f352ef4ef80b2417
Jesse Buffington <Bassmastry101@gmail.com>
2018-01-30 07:29:39 -0500

    Created caller scripts for php functions

---
+++ lib/syncEntities.php
@@ -0,0 +1,54 @@
+#!/bin/php
+<?php
+//
+//                  Y.                      _
+//                  YiL                   .```.
+//                  Yii;                .; .;;`.
+//                  YY;ii._           .;`.;;;; :
+//                  iiYYYYYYiiiii;;;;i` ;;::;;;;
+//              _.;YYYYYYiiiiiiYYYii  .;;.   ;;;
+//           .YYYYYYYYYYiiYYYYYYYYYYYYii;`  ;;;;
+//         .YYYYYYY$$YYiiYY$$$$iiiYYYYYY;.ii;`..
+//        :YYY$!.  TYiiYY$$$$$YYYYYYYiiYYYYiYYii.
+//        Y$MM$:   :YYYYYY$!"``"4YYYYYiiiYYYYiiYY.
+//     `. :MM$$b.,dYY$$Yii" :'   :YYYYllYiiYYYiYY
+//  _.._ :`4MM$!YYYYYYYYYii,.__.diii$$YYYYYYYYYYY
+//  .,._ $b`P`     "4$$$$$iiiiiiii$$$$YY$$$$$$YiY;
+//     `,.`$:       :$$$$$$$$$YYYYY$$$$$$$$$YYiiYYL
+//      "`;$$.    .;PPb$`.,.``T$$YY$$$$YYYYYYiiiYYU:
+//      ;$P$;;: ;;;;i$y$"!Y$$$b;$$$Y$YY$$YYYiiiYYiYY
+//      $Fi$$ .. ``:iii.`-":YYYYY$$YY$$$$$YYYiiYiYYY
+//      :Y$$rb ````  `_..;;i;YYY$YY$$$$$$$YYYYYYYiYY:
+//       :$$$$$i;;iiiiidYYYYYYYYYY$$$$$$YYYYYYYiiYYYY.
+//        `$$$$$$$YYYYYYYYYYYYY$$$$$$YYYYYYYYiiiYYYYYY
+//        .i!$$$$$$YYYYYYYYY$$$$$$YYY$$YYiiiiiiYYYYYYY
+//       :YYiii$$$$$$$YYYYYYY$$$$YY$$$$YYiiiiiYYYYYYi'
+//
+//          XOXO
+//  		Jesse B.
+//
+
+include '../includes/appConfig.php';
+include '../includes/functions.php';
+
+//configure command line arguments
+if($argc > 0){
+  foreach($argv as $arg){
+    $args = explode('=',$arg);
+    switch($args[0]){
+      case '--help':
+      return displayUsage();
+      case '--log':
+      $log = $args[1];
+      break;
+    }
+  }
+}
+
+while(true) {
+  exec(syncEntities());
+  sleep(interval_syncEntities);
+}
+
+
+?>
