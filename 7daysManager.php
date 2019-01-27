e4d31601df97ab5b88c7bd3780f5b03f46f56621
Jesse Buffington <jessebuffington1@gmail.com>
2019-01-26 19:20:08 -0500

    - Replaced static nohup upstart commands to use functions that will only start app processes that are enabled in DB.

--- 7daysManager.php
+++ 7daysManager.php
@@ -37,102 +37,37 @@
     foreach($argv as $arg){
       $args = explode('=',$arg);
       switch($args[0]){
-        case '--help':
-        return displayUsage();
-        case '--log':
+        //case 0;
+          //return displayUsage();
+        case '-h':
+          return displayUsage();
+        case '--help';
+          return displayUsage();
+        case 'help';
+          return displayUsage();
+        case '--stop';
+          return stopProcesses();
+        case 'stop';
+          return stopProcesses();
+        case '-r':
+          return restartProcesses();
+        case '--restart';
+          return restartProcesses();
+        case 'restart';
+          return restartProcesses();
+        case '--start';
+          return startProcesses();
+        case 'start';
+          return startProcesses();
+        case '-m';
+          return monitorAppStatus();
+        case '--monitor';
+          return monitorAppStatus();
+        case 'monitor';
+          return monitorAppStatus();
         $log = $args[1];
         break;
       }
     }
   }
-
-/*
-  ini_set('display_errors',1);
-  print "Parent : ". getmypid() . "\n";
-
-  global $pids;
-  $pids = Array();
-
-  // Daemonize
-  $pid = pcntl_fork();
-  if($pid){
-   // Only the parent will know the PID. Kids aren't self-aware
-   // Parent says goodbye!
-   print "\tParent : " . getmypid() . " exiting\n";
-   exit();
-  }
-
-  print "Child : " . getmypid() . "\n";
-
-  // Handle signals so we can exit nicely
-  declare(ticks = 1);
-  function sig_handler($signo){
-   global $pids,$pidFileWritten;
-   if ($signo == SIGTERM || $signo == SIGHUP || $signo == SIGINT){
-   // If we are being restarted or killed, quit all children
-
-   // Send the same signal to the children which we recieved
-   foreach($pids as $p){ posix_kill($p,$signo); }
-
-   // Women and Children first (let them exit)
-   foreach($pids as $p){ pcntl_waitpid($p,$status); }
-   print "Parent : "
-   .  getmypid()
-   . " all my kids should be gone now. Exiting.\n";
-   exit();
-   }else if($signo == SIGUSR1){
-   print "I currently have " . count($pids) . " children\n";
-   }
-  }
-  // setup signal handlers to actually catch and direct the signals
-  pcntl_signal(SIGTERM, "sig_handler");
-  pcntl_signal(SIGHUP,  "sig_handler");
-  pcntl_signal(SIGINT, "sig_handler");
-  pcntl_signal(SIGUSR1, "sig_handler");
-
-  // The program to launch
-  $program = "lib/syncAllPlayers.php";
-  $arguments = Array("");
-
-  while(TRUE){
-   if(count($pids) < 6){
-   $pid=pcntl_fork();
-   if(!$pid){
-   pcntl_exec($program,$arguments); // takes an array of arguments
-   exit();
-   } else {
-   $pids[] = $pid;
-   }
-   }
-
-   // Collect any children which have exited on their own. pcntl_waitpid will
-   // return the PID that exited or 0 or ERROR
-   // WNOHANG means we won't sit here waiting if there's not a child ready
-   // for us to reap immediately
-   // -1 means any child
-   $dead_and_gone = pcntl_waitpid(-1,$status,WNOHANG);
-   while($dead_and_gone > 0){
-   // Remove the gone pid from the array
-   unset($pids[array_search($dead_and_gone,$pids)]);
-
-   // Look for another one
-   $dead_and_gone = pcntl_waitpid(-1,$status,WNOHANG);
-   }
-
-   // Sleep for 1 second
-   sleep(1);
-  }
-*/
-
-shell_exec('nohup php -f lib/syncAllPlayers.php >> var/log/syncAllPlayers.log 2>&1 &');
-//shell_exec('nohup php -f lib/syncEntities.php >> var/log/syncEntities.log 2>&1 &');
-//shell_exec('nohup php -f lib/syncGameLog.php >> var/log/syncGameLog.log 2>&1 &');
-shell_exec('nohup php -f lib/syncGameTime.php >> var/log/syncGameTime.log 2>&1 &');
-shell_exec('nohup php -f lib/syncGameVersion.php >> var/log/syncGameVersion.log 2>&1 &');
-//shell_exec('nohup php -f lib/syncLandclaims.php >> var/log/syncLandClaims.log 2>&1 &');
-shell_exec('nohup php -f lib/syncOnlinePlayers.php >> var/log/syncOnlinePlayers.log 2>&1 &');
-shell_exec('nohup php -f lib/syncServerInfo.php >> var/log/syncServerInfo.log 2>&1 &');
-shell_exec('nohup php -f lib/insertPlayerHistory.php > var/log/insertPlayerHistory.log 2>&1 &');
-shell_exec('nohup php -f lib/syncGameChat.php > var/log/syncGameChat.log 2>&1 &');
-
 ?>
