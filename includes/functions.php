b6897e20bfd283bf5888825be3ba4df48896d7d3
Jesse Buffington <jessebuffington1@gmail.com>
2019-01-29 00:28:27 -0500

    - Removed colors in favor of the script written by Sallar Kaboli

--- includes/functions.php
+++ includes/functions.php
@@ -4,40 +4,6 @@ function __autoload($class) {
 }
 
 
-function echos($text, $color="normal") {
-  static $colors = array(
-    'light_red' => "[1;31m",
-		'light_green' => "[1;32m",
-		'yellow' => "[1;33m",
-		'light_blue' => "[1;34m",
-		'magenta' => "[1;35m",
-		'light_cyan' => "[1;36m",
-		'white' => "[1;37m",
-		'normal' => "[0m",
-		'black' => "[0;30m",
-		'red' => "[0;31m",
-		'green' => "[0;32m",
-		'brown' => "[0;33m",
-		'blue' => "[0;34m",
-		'cyan' => "[0;36m",
-		'bold' => "[1m",
-		'underscore' => "[4m",
-		'reverse' => "[7m"
-	);
-	$str = chr(27) . $colors[$color] . $text . chr(27) . "[0m";
-	if (false === FORKED) {
-    echo $str;
-		return;
-	}
-  if (Main::$screen === null) {
-    return;
-	}
-  if (false === @fwrite(Main::$screen, $str)) {
-    Main::$screen = null;
-  }
-}
-
-
 //Method for displaying the help and default variables.
 function displayUsage() {
   global $APP_LOG;
